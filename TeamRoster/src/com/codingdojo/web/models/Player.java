984f959e3b9a3028cf4f4c8798acd48972450658
Tonyphangluangmalay <50527802+Tonyphangluangmalay@users.noreply.github.com>
2019-08-05 06:50:57 -0500

    Add files via upload

---
+++ TeamRoster/src/com/codingdojo/web/models/Player.java
@@ -0,0 +1,27 @@
+package com.codingdojo.web.models;
+
+public class Player {
+	private String first_name;
+	private String last_name;
+	private Integer age;
+	
+	
+	public Player(String first_name, String last_name, Integer age) {
+		
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.age = age;
+	}
+	
+	public String getfirst_name() {
+		return first_name;
+	}
+	
+	public String getlast_name() {
+		return last_name;
+	}
+	
+	public Integer getAge() {
+		return age;
+	}
+}
